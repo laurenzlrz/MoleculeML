7e02b6b619fd6ff256401fc60854923851025579
laurenzlrz <laurenz.wurzel@gmail.com>
2025-01-04 12:34:45 +0100

    Outsorced general purpose enums and further.

---
+++ src/general/Property.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Property(Enum):
+    TOTAL_ENERGY = 'total_energy',
+    POTENTIAL_ENERGY = 'potential_energy',
+    KINETIC_ENERGY = 'kinetic_energy',
+    FORCES = 'forces',
+    DIPOLE = 'dipole',
+    ELEMENTS = 'elements',
+    COORDINATES = 'coordinates',
---
+++ src/general/Units.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Units(Enum):
+    CHARGE = 'charge',
+    ANGSTROM = 'ang',
+    KCALPERMOL = 'kcal/mol',
+    KCALPERMOLANGSTROM = 'kcal/mol/ang',
+    EV = 'eV',
